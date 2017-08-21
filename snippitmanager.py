0052deb10b50aaf1e4ae240bcb23131e125cbeae
Ashley-P <ashleyp1244@hotmail.co.uk>
2017-08-21 03:32:24 +0100

    Creating the GUI for the second window

--- snippitmanager.py
+++ snippitmanager.py
@@ -47,6 +47,10 @@ class Manager(tk.Frame):
         self.grid(sticky='nsew')
         self.init_gui()
 
+    def new_window(self, code_class=None):
+        self.win = AddEditWindow(code_class)
+        self.win.grab_set()
+
     def init_gui(self):
         # Making the frame stick to the window
         self.top = self.winfo_toplevel()
@@ -83,7 +87,7 @@ class Manager(tk.Frame):
         self.text_frame.grid_rowconfigure(1, weight=0)
 
         # Buttons
-        tk.Button(self.button_frame, text='Add', width=BUTTON_WIDTH, command=None).pack()
+        tk.Button(self.button_frame, text='Add', width=BUTTON_WIDTH, command=lambda:self.new_window()).pack()
         tk.Button(self.button_frame, text='Remove', width=BUTTON_WIDTH, command=None).pack()
         tk.Button(self.button_frame, text='Edit', width=BUTTON_WIDTH, command=None).pack()
         tk.Button(self.button_frame, text='Copy', width=BUTTON_WIDTH, command=None).pack()
@@ -104,6 +108,7 @@ class Manager(tk.Frame):
 
         self.code_text = tk.Text(self.text_frame, yscrollcommand=self.code_text_yscroll.set,
                                                   xscrollcommand=self.code_text_xscroll.set, 
+                                                  wrap="none",
                                                   state=tk.DISABLED,
                                                   width=BOX_WIDTH,
                                                   height=LINE_HEIGHT)
@@ -112,8 +117,8 @@ class Manager(tk.Frame):
         self.code_text_xscroll.config(command=self.code_text.xview)
 
         self.code_text.grid(column=0, row=0, sticky='nsew')
-        self.code_text_yscroll.grid(column=1, row=0, sticky='e')
-        self.code_text_xscroll.grid(column=0, row=1, sticky='s')
+        self.code_text_yscroll.grid(column=1, row=0, sticky='nse')
+        self.code_text_xscroll.grid(column=0, row=1, sticky='sew')
 
         # Entry
         self.search_bar = ttk.Entry(self.search_frame)
@@ -134,6 +139,89 @@ class Manager(tk.Frame):
                 child.grid_configure(padx=1, pady=1)
             except:
                 pass
+
+
+class AddEditWindow(tk.Toplevel):
+    '''Joint window for adding and editing code snippits'''
+    def __init__(self, code_class, master=None):
+        tk.Toplevel.__init__(self, master)
+        self.code_class = code_class
+        self.grid()
+        self.init_gui()
+
+    def init_gui(self):
+        self.top = self.winfo_toplevel()
+        self.top.grid_rowconfigure(0, weight=1)
+        self.top.grid_columnconfigure(0, weight=1)
+
+        # Frames
+        self.main_frame = tk.Frame(self)
+        self.main_frame.grid(column=0, row=0, padx=5, pady=5, sticky='nsew')
+
+        self.main_frame.grid_columnconfigure(0, weight=0)
+        self.main_frame.grid_columnconfigure(1, weight=1)
+
+        self.main_frame.grid_rowconfigure(0, weight=0)
+        self.main_frame.grid_rowconfigure(1, weight=1)
+        self.main_frame.grid_rowconfigure(2, weight=1)
+
+        
+        self.description_frame = tk.Frame(self.main_frame)
+        self.description_frame.grid(column=1, row=1, pady=5, sticky='nsew')
+
+        self.description_frame.grid_columnconfigure(0, weight=1)
+        self.description_frame.grid_rowconfigure(0, weight=1)
+
+
+        self.code_frame = tk.Frame(self.main_frame)
+        self.code_frame.grid(column=1, row=2, pady=5, sticky='nsew')
+
+        self.code_frame.grid_columnconfigure(0, weight=1)
+        self.code_frame.grid_rowconfigure(0, weight=1)
+
+        # Labels
+        tk.Label(self.main_frame, text='Name:').grid(column=0, row=0, padx=5)
+        tk.Label(self.main_frame, text='Description:').grid(column=0, row=1, padx=5)
+        tk.Label(self.main_frame, text='Code:').grid(column=0, row=2, padx=5)
+        
+        # Entry
+        self.name_entry = tk.Entry(self.main_frame)
+        self.name_entry.grid(column=1, row=0, sticky='ew')
+
+        # Description Text box
+        self.description_text_yscroll = tk.Scrollbar(self.description_frame, orient=tk.VERTICAL)
+        self.description_text_xscroll = tk.Scrollbar(self.description_frame, orient=tk.HORIZONTAL)
+
+        self.description_text = tk.Text(self.description_frame, yscrollcommand=self.description_text_yscroll.set,
+                                                                xscrollcommand=self.description_text_xscroll.set, 
+                                                                wrap="none",
+                                                                width=BOX_WIDTH,
+                                                                height=LINE_HEIGHT)
+
+        self.description_text_yscroll.config(command=self.description_text.yview)
+        self.description_text_xscroll.config(command=self.description_text.xview)
+
+        self.description_text.grid(column=0, row=0, sticky='nsew')
+        self.description_text_yscroll.grid(column=1, row=0, sticky='nse')
+        self.description_text_xscroll.grid(column=0, row=1, sticky='sew')
+
+        # Code Text Box
+        self.code_text_yscroll = tk.Scrollbar(self.code_frame, orient=tk.VERTICAL)
+        self.code_text_xscroll = tk.Scrollbar(self.code_frame, orient=tk.HORIZONTAL)
+
+        self.code_text = tk.Text(self.code_frame, yscrollcommand=self.code_text_yscroll.set,
+                                                  xscrollcommand=self.code_text_xscroll.set, 
+                                                  wrap="none",
+                                                  width=BOX_WIDTH,
+                                                  height=LINE_HEIGHT)
+
+        self.code_text_yscroll.config(command=self.code_text.yview)
+        self.code_text_xscroll.config(command=self.code_text.xview)
+
+        self.code_text.grid(column=0, row=0, sticky='nsew')
+        self.code_text_yscroll.grid(column=1, row=0, sticky='nse')
+        self.code_text_xscroll.grid(column=0, row=1, sticky='sew')
+
 
 
 if __name__ == "__main__":
